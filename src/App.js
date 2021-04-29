17a93aea9ddf4e6d07c170f5ab57ef3b4e27125e
tatendabakozw <tatendabakozw@gmail.com>
2021-04-29 12:37:29 +0200

    navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Nav from './Nav';
 import useDarkMode from './useDarkMode';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 
 function App() {
-
   useDarkMode()
-
   return (
-    <div className="App bg-gray-500 dark:bg-gray-900 h-screen">
-      <div>        
-        <Nav/>
-      </div>
-      <h1 className="text-center text-black dark:text-gray-50">Tailwind Css Boilerplate</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className="App bg-gray-500 dark:bg-gray-900 h-screen">
+          <div>
+            <Nav />
+          </div>
+          <h1 className="text-center text-black dark:text-gray-50">Tailwind Css Boilerplate</h1>
+        </div>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
